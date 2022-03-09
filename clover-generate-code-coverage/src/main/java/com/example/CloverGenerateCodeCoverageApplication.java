b78cf1fcc16aacf8208491eb6f14a74c90addafb
cloudtechmasters <cloudtechmasters@gmail.com>
2022-03-09 08:02:33 +0530

    added gradle project

---
+++ clover-generate-code-coverage/src/main/java/com/example/CloverGenerateCodeCoverageApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloverGenerateCodeCoverageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloverGenerateCodeCoverageApplication.class, args);
+	}
+
+}
